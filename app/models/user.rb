714f5b9af52ee21b79df12f10b06a4e5895eac6e
juuata <tomoki19980522@gmail.com>
2022-12-23 16:55:09 +0900

    単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name, presence: true
+    validates :first_name, presence: true
     validates :family_name_kana, presence: true
     validates :first_name_kana, presence: true
   end
