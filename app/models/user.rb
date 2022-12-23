69747ac32e8ab3b7d2237ec8ac686224effe7ffb
juuata <tomoki19980522@gmail.com>
2022-12-23 18:00:11 +0900

    ユーザー管理機能の実装　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :family_name, presence: true
