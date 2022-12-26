be447fd2033ecb958a68d3dc38a1bec9bcdfbd71
juuata <tomoki19980522@gmail.com>
2022-12-26 18:46:28 +0900

    カラムの作成・createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+  def new
+    @items = Item.new
+  end
+
+  def  create
+    item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:items_name, :description, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :days_for_shipping_id, :price, :user)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  validates :items_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_for_shipping_id, presence: true
+  validates :price, presence: true
+   
 end
