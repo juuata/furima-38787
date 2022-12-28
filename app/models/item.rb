ff9605b15f28c464d900a252d6e66e942673b2ea
juuata <tomoki19980522@gmail.com>
2022-12-28 12:02:13 +0900

    価格のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,7 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :days_for_shipping_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :image, presence: true
    
 end
